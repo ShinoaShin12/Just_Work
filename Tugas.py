fc732f052ea46a8c73370d9c0204d39ede70ab7f
ShinoaShin12 <126218727+ShinoaShin12@users.noreply.github.com>
2023-03-19 23:12:00 +0700

    Update Tugas.py

--- Tugas.py
+++ Tugas.py
@@ -68,15 +68,3 @@ col1.line_chart(WR)
 
 col2.subheader("A narrow column with the data")
 col2.write(df)
-
-st.subheader("Latihan 2 : Database ")
-@st.cache_data(ttl=600)
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
-
-df = load_data(st.secrets["public_gsheets_url"])
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a score :{row.UAS}:")
