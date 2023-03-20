6d9c4604f79d377ec269f7d5f320f445445d6ad7
ShinoaShin12 <126218727+ShinoaShin12@users.noreply.github.com>
2023-03-20 17:11:29 +0700

    Update Tugas_3.py

--- Tugas_3.py
+++ Tugas_3.py
@@ -75,3 +75,6 @@ my_bar = st.progress(0, text=progress_text)
 for percent_complete in range(100):
     time.sleep(0.1)
     my_bar.progress(percent_complete + 1, text=progress_text)
+for percent_complete in range(75):
+    time.sleep(0.1)
+    my_bar.progress(percent_complete + 1, text=progress_text)
