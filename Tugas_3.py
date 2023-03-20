37b432af608fa9b2c60983cb48eb1ba9bed1ab7b
ShinoaShin12 <126218727+ShinoaShin12@users.noreply.github.com>
2023-03-20 16:30:28 +0700

    Update Tugas_3.py

--- Tugas_3.py
+++ Tugas_3.py
@@ -6,7 +6,7 @@ import numpy as np
 # Tugas 3
 '''
 
-st.subheader("1. Write and Magic")
+st.subheader("1. Write and Magic :")
 
 st.subheader("Write : ")
 st.write('Hello William')
@@ -15,3 +15,13 @@ st.subheader("Magic : ")
 '''
 # *_Hello William_*
 '''
+
+st.subheader("2. Text Element :")
+st.markdown('This Game is **_really_ cool**.')
+
+st.subheader("3. Data Dispaly Element :")
+df = pd.DataFrame(
+   np.random.randn(50, 20),
+   columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)
